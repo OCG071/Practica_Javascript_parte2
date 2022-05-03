07fc5db7e45fe108a5df563c10896cd7b7c39974
José Juan Rocha Cisneros <19031005@itcelaya.edu.mx>
2022-05-03 11:08:26 -0500

    Se agrega funcionamiento de checklist, almacenamiento en el navegador y un modal para editar

--- js/components/add-todo.js
+++ js/components/add-todo.js
@@ -1,16 +1,21 @@
-class AddTodo{
+import Alert from './alert.js';
+
+export default class AddTodo{
     constructor(){
         this.btn = document.getElementById('add');
         this.title = document.getElementById('title');
         this.description = document.getElementById('description');
+
+        this.Alert = new Alert()
     }
 
     OnClick(callback){
         this.btn.onclick = () =>{
             if(this.title.value === '' || description.value === ''){
                // alert.innerText = 'Title and description are required';
-               console.error('Incorrecto');
+               this.Alert.show('Title and description are required');
             }else{
+                this.Alert.hide();
                 callback(this.title.value, this.description.value);
             }
         }
