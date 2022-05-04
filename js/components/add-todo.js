9e38ff6bfc2c424c8ecccd4a998c6d7d29dba82a
DanielPerezM <19031059@itcelaya.edu.mx>
2022-05-03 21:20:03 -0500

    Se completa la función de Local Storage y se agrega funcionalidad de filtros corregido

--- js/components/add-todo.js
+++ js/components/add-todo.js
@@ -1,23 +1,22 @@
 import Alert from './alert.js';
 
-export default class AddTodo{
-    constructor(){
-        this.btn = document.getElementById('add');
-        this.title = document.getElementById('title');
-        this.description = document.getElementById('description');
+export default class AddTodo {
+  constructor() {
+    this.btn = document.getElementById('add');
+    this.title = document.getElementById('title');
+    this.description = document.getElementById('description');
 
-        this.Alert = new Alert()
-    }
+    this.alert = new Alert('alert');
+  }
 
-    OnClick(callback){
-        this.btn.onclick = () =>{
-            if(this.title.value === '' || description.value === ''){
-               // alert.innerText = 'Title and description are required';
-               this.Alert.show('Title and description are required');
-            }else{
-                this.Alert.hide();
-                callback(this.title.value, this.description.value);
-            }
-        }
+  onClick(callback) {
+    this.btn.onclick = () => {
+      if (title.value === '' || description.value === '') {
+        this.alert.show('Title and description are required');
+      } else {
+        this.alert.hide();
+        callback(this.title.value, this.description.value);
+      }
     }
+  }
 }
--- js/components/modal.js
+++ js/components/modal.js
@@ -1,36 +1,37 @@
-import Alert from "./alert.js";
+import Alert from './alert.js';
+
 export default class Modal {
-    constructor() {
-        this.title = document.getElementById('modal-title');
-        this.description = document.getElementById('modal-description');
-        this.btn = document.getElementById('modal-btn');
-        this.completed = document.getElementById('modal-completed');
-        this.Alert = new Alert('modal-alert')
-        this.todo = null;
-    }
+  constructor() {
+    this.title = document.getElementById('modal-title');
+    this.description = document.getElementById('modal-description');
+    this.btn = document.getElementById('modal-btn');
+    this.completed = document.getElementById('modal-completed');
+    this.alert = new Alert('modal-alert');
 
-    setValues(todo) {
-        this.todo = todo;
-        this.title.value = todo.title;
-        this.description.value = todo.description;
-        this.completed.checked = todo.completed;
-    }
+    this.todo = null;
+  }
 
-    onClick(callback) {
-        this.btn.onclick = () =>{
-            if(!this.title.value || !this.description.value){
-               // alert.innerText = 'Title and description are required';
-               this.Alert.show('Title and description are required');
-               return;
-            }
+  setValues(todo) {
+    this.todo = todo;
+    this.title.value = todo.title;
+    this.description.value = todo.description;
+    this.completed.checked = todo.completed;
+  }
 
-            $('#modal').modal('toggle');
-           
-            callback(this.todo.id,{
-             title: this.title.value,
-             description: this.description.value,
-             completed: this.completed.checked,
-            });
-        }
+  onClick(callback) {
+    this.btn.onclick = () => {
+      if (!this.title.value || !this.description.value) {
+        this.alert.show('Title and description are required');
+        return;
+      }
+
+      $('#modal').modal('toggle');
+
+      callback(this.todo.id, {
+        title: this.title.value,
+        description: this.description.value,
+        completed: this.completed.checked,
+      });
     }
+  }
 }
